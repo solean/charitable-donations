853afdf3a313058e46ac8a35b69fac5632705cf7
Chris Schnabel <cschnabel@softrek.com>
2022-05-29 23:37:17 -0400

    Hakuai name changes

--- truffle/test/TestCharitableDonations.sol
+++ truffle/test/TestCharitableDonations.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.14;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/CharitableDonations.sol";
+import "../contracts/Hakuai.sol";
 
-contract TestCharitableDonations {
+contract TestHakuai {
 
   // function testCreatePledge() {
-  //   CharitableDonations cd = CharitableDonations(DeployedAddresses.CharitableDonations());
-  //   cd.createPledge(0.1 ether, 1 ether, 1 hours, address(0xFE3D9E533B81b497DF5eCab9b44db93175eF200E)){ value: 0.1 ether };
+  //   Hakuai h = Hakuai(DeployedAddresses.Hakuai());
+  //   h.createPledge(0.1 ether, 1 ether, 1 hours, address(0xFE3D9E533B81b497DF5eCab9b44db93175eF200E)){ value: 0.1 ether };
     
   //   Assert.equal(cd.pledges[1].duration, 1 hours, "Duration should be 1 hour");
   // }
